ae2a09172deb6a2c8a7fbc9d9c39416b4fa9aeeb
ajackrel <ajackrel@stratfordschools.com>
2023-10-10 14:27:48 -0700

    first commit

---
+++ app/src/main/java/com/example/iampoorpart5/MainActivity.java
@@ -0,0 +1,20 @@
+/*
+This is a test.
+Please answer the following question, then commit+push to your repos
+Is cereal soup? Yes or no.
+ */
+
+package com.example.iampoorpart5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
