ae228f086470eb67b887b1ea23e39ed26d1520db
ajackrel <ajackrel@stratfordschools.com>
2023-10-10 14:27:48 -0700

    second commit

--- app/src/main/java/com/example/iampoorpart5/MainActivity.java
+++ app/src/main/java/com/example/iampoorpart5/MainActivity.java
@@ -1,7 +1,38 @@
 /*
-This is a test.
-Please answer the following question, then commit+push to your repos
-Is cereal soup? Yes or no.
+AUTHOR:
+DATE:
+ASSIGNMENT:
+ */
+
+/*
+I AM POOR PART 5
+Continuation of part 4. For this app, you will create a app with the following requirements:
+-use a constraint layout
+
+LAYOUT:
+-TextView at the top of the screen
+-Linear Layout (horizontal):
+    -2 buttons, I AM RICH and I AM POOR, that are:
+        -equal distance from each other
+        -equal distance to the edges of the screen
+        -centered text
+        -TIP: USE GRAVITY TO CENTER and the View XML code we went over in class
+-ImageView at the bottom of the screen
+
+APP BEHAVIOUR:
+-When user presses the I AM RICH button:
+    -the text will change to "I AM RICH"
+    -the image will change to a diamond
+-When user presses the I AM POOR button:
+    -the text will change to "I AM POOR"
+    -the image will change to a coal
+
+CODE:
+-create a new method called verifyRichness(Button button) that does the following:
+    -takes a button as an argument
+    -checks the text of the button (TIP: use .getText() and don't forget to cast!)
+    -makes the appropriate appearance changes (text and image)
+
  */
 
 package com.example.iampoorpart5;
@@ -9,6 +40,7 @@ package com.example.iampoorpart5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,4 +49,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    public void verifyRichness(Button button) {
+
+    }
+
+    }
